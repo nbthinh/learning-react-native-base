5d659e6710bb5a5be8c545d932ddf3c1c91e98a0
nbthinh <nguyenbuuthinh036@gmail.com>
2024-09-20 19:18:11 +0700

    #6

--- App.tsx
+++ App.tsx
@@ -1,10 +1,46 @@
 import { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   const [count, setCount] = useState<number>(0);
+  const [name, setName] = useState<string>("");
+  const [age, setAge] = useState<number>(0);
   return (
     <View style={styles.container}>
+      <View>
+        <Text
+          style={{ fontSize: 40, fontWeight: "600" }}
+        >Name: {name}</Text>
+        <TextInput
+          multiline
+          autoCapitalize={"words"}
+          style={{
+            borderColor: "green",
+            borderWidth: 1,
+            width: 200,
+            padding: 15,
+          }} 
+          onChangeText={(value) => setName(value)}
+        />
+      </View>
+
+      <View>
+        <Text
+          style={{ fontSize: 40, fontWeight: "600" }}
+        >Age: {age}</Text>
+        <TextInput
+          style={{
+            borderColor: "green",
+            borderWidth: 1,
+            width: 200,
+            padding: 15,
+          }} 
+          onChangeText={(value) => setAge(+value)}
+          keyboardType="numeric"
+          maxLength={2}
+        />
+      </View>
+
       <Text style={{ fontSize: 40, fontWeight: "600" }}>
         count = {count}
       </Text>
