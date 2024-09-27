d55c05906dfae8cbcd030be3fea09543c0b1c38a
nbthinh <nguyenbuuthinh036@gmail.com>
2024-09-27 19:38:13 +0700

    #16

--- App.tsx
+++ App.tsx
@@ -7,6 +7,9 @@ import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import { OPENSANS_REGULAR } from "./utils/const";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 SplashScreen.preventAutoHideAsync();
 
 const App = () => {
@@ -21,12 +24,24 @@ const App = () => {
   if (!loaded && !error) {
     return null;
   }
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View>
-      <HomeScreen />
-      <DetailScreen />
-      <AboutScreen />
-    </View>
+    // <View>
+    //   <HomeScreen />
+    //   <DetailScreen />
+    //   <AboutScreen />
+    // </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="home"
+          component={HomeScreen}
+          options={{ title: "Trang chủ" }}
+        />
+        <Stack.Screen name="review-detail" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
--- components/review/home.tsx
+++ components/review/home.tsx
@@ -1,12 +1,22 @@
-import { View, Text } from "react-native";
-
+import { View, Text, Button } from "react-native";
 
 const HomeScreen = () => {
-    return (
-        <View>
-            <Text>HomeScreen</Text>
-        </View>
-    );
-}
+  return (
+    <View>
+      <Text style={{ fontSize: 40 }}>
+        thằng nghiêm văn sỹ lúc thì mạt sát người ta, lúc thì nói chuyện dễ
+        nghe, đúng là đa nhân cách
+      </Text>
+      <Button
+        title="View Detail"
+        onPress={() =>
+          alert(
+            "thằng nghiêm văn sỹ lúc thì mạt sát mình, lúc thì nó nói chuyện dễ nghe, đúng là đa nhân cách"
+          )
+        }
+      />
+    </View>
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
